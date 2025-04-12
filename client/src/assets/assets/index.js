1923ce1f45b5cf2855d360b35d3f5ad1b0c79583
GREG_ROGERS <GREGORYROGERS@FIRSTTEE.ORG>
2025-04-12 11:18:17 -0400

    master

---
+++ client/src/assets/assets/index.js
@@ -0,0 +1,27 @@
+import ai from "./ai.png";
+import fileIcon from "./file.png";
+import swatch from "./swatch.png";
+import download from "./download.png";
+import scaleicon from "../scaleicon.js";
+
+import logoShirt from "./logo-tshirt.png";
+import stylishShirt from "./stylish-tshirt.png";
+
+import shirtIcon3D from "./3d-shirt-icon.png";
+import hoodieIcon3D from "./3d-hoodie-icon.png";
+import bootIcon3D from "./3d-boot-icon.png";
+import sneakerIcon3D from "./3d-sneaker-icon.png";
+
+export {
+  ai,
+  fileIcon,
+  swatch,
+  download,
+  scaleicon,
+  logoShirt,
+  stylishShirt,
+  shirtIcon3D,
+  hoodieIcon3D,
+  bootIcon3D,
+  sneakerIcon3D,
+};
