00081fe20d8e80634092b960d5051964f7e891a0
YuryOAraujo <127779626+YuryOAraujo@users.noreply.github.com>
2025-04-14 01:04:19 -0300

    remove the base url to be able to render the home component

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/guide/dep-pre-bundling.html - important for builds.
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/PlaceYourPrintStudio',
-
   commonjsOptions: {
     esmExternals: true 
  },
