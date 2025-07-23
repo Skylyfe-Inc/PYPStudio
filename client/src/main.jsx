c7f34b4d098a4534007efb58726f8aab0b89d710
GREG_ROGERS <GREGORYROGERS@FIRSTTEE.ORG>
2025-07-22 21:48:09 -0400

    PYPSConfig Backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Amplify } from 'aws-amplify';
+import awsExports from '../aws-exports';
+
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
