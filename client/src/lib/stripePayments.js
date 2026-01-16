91f84f41e6f42739a2cfd1242946a2d57f85b2cc
timothylewis91 <timothyplewis91@yahoo.com>
2026-01-16 08:48:55 -0800

    package

--- client/src/lib/stripePayments.js
+++ client/src/lib/stripePayments.js
@@ -1,18 +1,5 @@
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStripePayments } from "@invertase/firestore-stripe-payments";
 import app from "../config/firebase";
-
-export function stripePayments() {
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-
-  // IMPORTANT: this points to the collections the extension uses
-  return getStripePayments(app, {
-    productsCollection: "products",
-    customersCollection: "customers",
-  });
-}
 
 export function getCurrentUserOrThrow() {
   const auth = getAuth(app);
